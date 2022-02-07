87cd76d17ec3738e2fab99f9d33404b689239da1
Chandan Dhamande <virtualassistance.py@gmail.com>
2022-02-07 21:34:04 +0530

    Create ifElse.sol

---
+++ ifElse.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.8.7
+
+contract ifElse {
+    function foo(uint x) public pure returns (uint) {
+        if (x < 10) {
+            return 0;
+        }
+        else if (x < 20) {
+            return 1;
+        }
+        else {
+            return 2;
+        }
+    }
+
+    function ternery(uint xBar) public pure returns (uint) {
+        return xBar < 10 ? 1: 2;
+    }
+}
